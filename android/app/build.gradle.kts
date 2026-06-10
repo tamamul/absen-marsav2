b88f9cf209bbcf36933649cb242a549c32962c1c
tamamul <36046825+tamamul@users.noreply.github.com>
2026-06-10 08:08:05 +0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,6 +27,8 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode()
         versionName = flutter.versionName()
+        }
+
 
     buildTypes {
         release {
